1a310d53aedf6fd18f8975d7aab56c6b319e5d24
itachi234 <88839257+itachi234@users.noreply.github.com>
2021-08-19 08:35:07 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ let minutos = 0
 let horas = 0
 basic.forever(function () {
     if (segundos < 59) {
-        segundos = segundos + 1
+        segundos += 1
     }
     if (segundos == 59) {
         minutos = minutos + 1
